6d7124af5959fdf9401b72e38f7973bec3d4896d
paradoxirony <coderbhaiya@gmail.com>
2024-04-25 20:17:41 +0530

    dn

---
+++ Newfolder/dishu.js
@@ -0,0 +1,42 @@
+// const accountId = 124323
+// let account_email = "hitesh@gmail.com"
+// var accountpassword = "121212"
+// // accountId =2
+// // console.log(accountId);
+// console.table([accountId,account_email,accountpassword]);
+// console.log(3+3);
+// console.log(typeof "dishu");
+let somenumber = 33
+let some_other_number = String(somenumber)
+// console.log(some_other_number);
+// console.log(typeof some_other_number);
+
+/* some complex things going to be happend */
+// console.log("1"+2);
+// console.log(typeof (1+"3"));
+// console.log("1"+2+2);
+// console.log(1+2+"3");
+
+// const a =1 ;
+// const b =2;
+// let d = 3
+// // // let tr = a
+// // // console.log(typeof a+ " "+b);
+// console.log(++d);
+// console.log(++d);
+const gamename = new String('dishugawri')
+// console.log(gamename.__proto__);
+// console.log(gamename[1]);
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.toLowerCase());
+// console.log(gamename.toLocaleLowerCase());
+// console.log(gamename.startsWith("d"));
+// console.log(gamename.endsWith("i"));
+// console.log(gamename.strike());
+// console.log(gamename.substring([0]));
+// console.log(gamename.charAt(3))
+// console.log(gamename.indexOf('i'));
+// const newsre = gamename.substring(0,4);
+const sliceclearh = gamename.slice(0,5);
+console.log(sliceclearh);
