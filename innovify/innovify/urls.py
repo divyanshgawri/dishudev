6f5e7d731b7a032b3c1969d746860a258d2ba3cd
divyanshgawri <dishugawri@gmail.com>
2024-05-12 14:57:39 +0530

    dn

--- innovify/innovify/urls.py
+++ innovify/innovify/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from innovify_app import views
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('signup',views.signup,name='signup')
 ]
