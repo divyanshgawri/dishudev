ef32176d2eeb5d327b063f458af37c3a2154004a
divyanshgawri <142962004+divyanshgawri@users.noreply.github.com>
2024-05-13 15:20:26 +0530

    Update settings.py

--- innovify/innovify/settings.py
+++ innovify/innovify/settings.py
@@ -121,4 +121,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-STATICFILES_DIRS = [BASE_DIR , "static",]
+STATICFILES_DIRS = [BASE_DIR , "static","innovify"]
